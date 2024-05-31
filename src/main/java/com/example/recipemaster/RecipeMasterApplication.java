4ee83de9995420de5895572327be5668591b1c12
Seungpil Huh <gjtmdvlf357@gmail.com>
2024-05-31 17:31:10 +0900

    2인1조 팀 프로젝트

---
+++ src/main/java/com/example/recipemaster/RecipeMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipemaster/RecipeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
