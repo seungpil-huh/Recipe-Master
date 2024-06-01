c9d8d1c802772b638ec8e80427892028490cc511
dongwoobae <https://github.com/dongwoobae/naver0314devops11.git>
2024-06-01 10:47:49 +0900

    기본 세팅 조금 한거 PR

--- src/main/java/com/example/recipemaster/RecipeMasterApplication.java
+++ src/main/java/com/example/recipemaster/RecipeMasterApplication.java
@@ -1,9 +1,12 @@
 package com.example.recipemaster;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cloud","com.example.*","data.*","controller.*","config"})
+@MapperScan("data.mapper")
 public class RecipeMasterApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/recipemaster/WebCrawler.java
+++ src/main/java/com/example/recipemaster/WebCrawler.java
@@ -1,4 +1,4 @@
 package com.example.recipemaster;
 
-public class TestBoo {
+public class WebCrawler {
 }
