f68ad932816a969577bfba56b74be9ddf84e0559
Seungpil Huh <gjtmdvlf357@gmail.com>
2024-06-02 15:59:48 +0900

    허승필 입니다! (0602 오후 4시 제출)

--- src/main/java/com/example/recipemaster/RecipeMasterApplication.java
+++ src/main/java/com/example/recipemaster/RecipeMasterApplication.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication(scanBasePackages = {"cloud","com.example.*","data.*","controller.*","config"})
 @MapperScan("data.mapper")
