6bfd267e3aa593679029c4b94101680a0f833e92
Seungpil Huh <gjtmdvlf357@gmail.com>
2024-06-05 11:25:18 +0900

    허승필 입니다!

--- src/main/java/data/mapper/BoardDetailInter.java
+++ src/main/java/data/mapper/BoardDetailInter.java
@@ -1,13 +1,17 @@
 package data.mapper;
 
 import data.dto.BoardDto;
+import data.dto.RecipeReviewDto;
 import data.dto.UserRecipeDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface BoardDetailInter {
     public BoardDto selectOneProRecipe(Map<String,Object> map);
     public UserRecipeDto selectOneUserRecipe(Map<String,Object> map);
+    public void insertRecipeReview(RecipeReviewDto reviewDto);
+    public List<RecipeReviewDto> selectReviewsByRecipeId(int recipe_id);
 }
